32913d7b841c42f2875ddd34173666a724cff91c
gravic <grant@gravicdesign.com>
2026-04-09 11:13:20 -0400

    added client detail page

--- complete_business_analysis_tool/clients/urls.py
+++ complete_business_analysis_tool/clients/urls.py
@@ -2,12 +2,13 @@
 
 from django.urls import path
 
-from .views import ClientCreateView, ClientListView, ClientUpdateView
+from .views import ClientCreateView, ClientDetailView, ClientListView, ClientUpdateView
 
 app_name = "clients"
 
 urlpatterns = [
     path("", view=ClientListView.as_view(), name="list"),
     path("create/", view=ClientCreateView.as_view(), name="create"),
+    path("<uuid:pk>/", view=ClientDetailView.as_view(), name="detail"),
     path("<uuid:pk>/edit/", view=ClientUpdateView.as_view(), name="edit"),
 ]
--- complete_business_analysis_tool/clients/views.py
+++ complete_business_analysis_tool/clients/views.py
@@ -1,6 +1,8 @@
-from django.db.models import Count
+from django.db.models import Count, OuterRef, Subquery, Sum
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView, UpdateView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
+
+from complete_business_analysis_tool.assessments.models import Answer
 
 from .forms import ClientForm
 from .models import Client
@@ -21,6 +23,30 @@ class ClientCreateView(CreateView):
     form_class = ClientForm
     template_name = "pages/clients/client-form.html"
     success_url = reverse_lazy("clients:list")
+
+
+class ClientDetailView(DetailView):
+    model = Client
+    template_name = "pages/clients/client-detail.html"
+    context_object_name = "client"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        score_subquery = (
+            Answer.objects.filter(assessment=OuterRef("pk"))
+            .values("assessment")
+            .annotate(total=Sum("selected_option__rank"))
+            .values("total")
+        )
+        context["assessments"] = (
+            self.object.assessments.select_related("template")
+            .annotate(
+                question_count=Count("template__template_questions", distinct=True),
+                score=Subquery(score_subquery),
+            )
+            .order_by("-created_at")
+        )
+        return context
 
 
 class ClientUpdateView(UpdateView):
